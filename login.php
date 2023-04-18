e436d6c0004588a5fcb5aa20bc48eaff6ee8ddd8
Roumyadeb Karmakar <roumya.karmakar.01@gmail.com>
2023-04-18 13:32:04 +0530

    updates

--- login.php
+++ login.php
@@ -106,7 +106,7 @@ if(!empty($_POST)){
                                     <label class="form-check-label" for="customCheck1">Remember me</label>
                                 </div>
                                 <div class="ms-auto">
-                                    <a href="javascript:void(0)" id="to-recover" class="text-muted"><i class="fas fa-lock m-r-5"></i> Forgot pwd?</a>
+                                    <a href="resetpassword.php" id="to-recover" class="text-muted"><i class="fas fa-lock m-r-5"></i> Forgot password?</a>
                                 </div>
                             </div>
                         </div>
@@ -123,7 +123,7 @@ if(!empty($_POST)){
                         </div>
                     </div>-->
                 </form>
-                <form class="form-horizontal" id="recoverform" action="index.html">
+                <form class="form-horizontal" id="recoverform" action="forgot-password.php">
                     <div class="form-group ">
                         <div class="col-xs-12">
                             <h3>Recover Password</h3>
@@ -132,13 +132,14 @@ if(!empty($_POST)){
                     </div>
                     <div class="form-group ">
                         <div class="col-xs-12">
-                            <input class="form-control" type="text" required="" placeholder="Email"> </div>
+                            <input class="form-control" type="text" required="" name="email" placeholder="Email"> </div>
                     </div>
                     <div class="form-group text-center m-t-20">
                         <div class="col-xs-12">
-                            <button class="btn btn-primary btn-lg w-100 text-uppercase waves-effect waves-light" type="submit">Reset</button>
+                            <button class="btn btn-primary btn-lg w-100 text-uppercase waves-effect waves-light" type="submit" name="submit_email">Reset</button>
                         </div>
                     </div>
+
                     <div class="form-group m-b-0">
                         <div class="col-sm-12 text-center">
                             <a href="javascript:void(0)" id="to-login" class="text-info m-l-5"><b> Go To Login Page </b></a>
