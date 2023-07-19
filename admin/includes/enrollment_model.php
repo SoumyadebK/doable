d0bf4962ea7b4a66a9c4e7be31492db7d26513e5
roumya <roumya.karmakar.01@gmail.com>
2023-07-19 12:01:32 +0530

    flexible payment

--- admin/includes/enrollment_model.php
+++ admin/includes/enrollment_model.php
@@ -553,7 +553,7 @@ $NOTE = '';
         modal.style.display = "none";
     }
 
-    // When the user clicks anywhere outside of the modal, close it
+    // When the user clicks anywhere out side of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
