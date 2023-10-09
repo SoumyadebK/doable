ed95a9fe4d91570a5d1b73b57fc2ac69ac187d4f
roumya <roumya.karmakar.01@gmail.com>
2023-10-10 00:23:43 +0530

    10-10

--- admin/pagination/enrollment_list.php
+++ admin/pagination/enrollment_list.php
@@ -222,7 +222,7 @@ while (!$row->EOF) {
                                 }
                                 ?>
                             </td>
-                            <td><?=$used_session_count->fields['USED_SESSION_COUNT']?></td>
+                            <td><?=$paid_session_count?></td>
                             <td><?=$serviceCodeData->fields['NUMBER_OF_SESSION']-$used_session_count->fields['USED_SESSION_COUNT']?></td>
                             <td><?=($total_bill_and_paid->fields['TOTAL_BILL']==0) ? 0 : $serviceCodeData->fields['NUMBER_OF_SESSION']-$used_session_count->fields['USED_SESSION_COUNT']?></td>
                         </tr>
