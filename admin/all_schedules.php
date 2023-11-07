92f1866f93dbf2cadfb872de16fe281f6597a4eb
unknown <deb.soumya93@gmail.com>
2023-11-07 23:19:42 +0530

    fixes

--- admin/all_schedules.php
+++ admin/all_schedules.php
@@ -164,13 +164,13 @@ if (isset($_GET['CHOOSE_DATE']) && $_GET['CHOOSE_DATE'] != '') {
     $CHOOSE_DATE = date("Y-m-d");
 }
 
-$business_type = $db->Execute("SELECT DOA_BUSINESS_TYPE.TIME_SLOT_INTERVAL FROM DOA_BUSINESS_TYPE INNER JOIN DOA_ACCOUNT_MASTER ON DOA_BUSINESS_TYPE.PK_BUSINESS_TYPE=DOA_ACCOUNT_MASTER.PK_BUSINESS_TYPE WHERE PK_ACCOUNT_MASTER=".$_SESSION['PK_ACCOUNT_MASTER']);
+/*$business_type = $db->Execute("SELECT DOA_BUSINESS_TYPE.TIME_SLOT_INTERVAL FROM DOA_BUSINESS_TYPE INNER JOIN DOA_ACCOUNT_MASTER ON DOA_BUSINESS_TYPE.PK_BUSINESS_TYPE=DOA_ACCOUNT_MASTER.PK_BUSINESS_TYPE WHERE PK_ACCOUNT_MASTER=".$_SESSION['PK_ACCOUNT_MASTER']);
 if ($business_type->fields['TIME_SLOT_INTERVAL'] == "00:00:00") {
     $INTERVAL = "00:15:00";
 }else {
     $INTERVAL = $business_type->fields['TIME_SLOT_INTERVAL'];
-}
-
+}*/
+$INTERVAL = "00:15:00";
 ?>
 
 <!DOCTYPE html>
@@ -564,6 +564,14 @@ if ($business_type->fields['TIME_SLOT_INTERVAL'] == "00:00:00") {
                 showAppointmentEdit(info);
                 // window.location.href = "add_schedule.php?id="+info.id;
                 //viewAppointmentDetails(info);
+            },
+
+            eventDrop: function (info) {
+                let PK_APPOINTMENT_MASTER = info.id;
+                let SERVICE_PROVIDER_ID = info.resourceId;
+                let START_DATE_TIME = info.start.toDate().toString();
+                let END_DATE_TIME = info.end.toDate().toString();
+                console.log(PK_APPOINTMENT_MASTER, SERVICE_PROVIDER_ID, START_DATE_TIME, END_DATE_TIME);
             },
 
             select: function(start, end, jsEvent, view, resource) {
