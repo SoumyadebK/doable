61891df5be73e4b149f78067e861d35889ac01f8
soumyadeb1993 <deb.soumya93@gmail.com>
2024-07-24 00:00:53 +0530

    fixes

--- includes/setup_menu_super_admin.php
+++ includes/setup_menu_super_admin.php
@@ -18,10 +18,7 @@
                                 <a class="dropdown-item" href="../super_admin/all_states.php">States</a>
                                 <a class="dropdown-item" href="../super_admin/all_currency.php">Currency</a>
                                 <a class="dropdown-item" href="../super_admin/all_account_types.php">Account Types</a>
-                                <a class="dropdown-item" href="../super_admin/all_appointment_status.php">Appointment Types</a>
                                 <a class="dropdown-item" href="../super_admin/all_business_types.php">Business Types</a>
-                                <a class="dropdown-item" href="../super_admin/csv_uploader.php">CSV Uploader</a>
-                                <a class="dropdown-item" href="../super_admin/all_chat_section.php">Chat Section</a>
                             </div>
                         </li>
                         <li class="nav-item dropdown">
@@ -32,6 +29,7 @@
                                 <a class="dropdown-item" href="../super_admin/all_enrollment_types.php">Enrollment Types</a>
                                 <a class="dropdown-item" href="../super_admin/all_agreement_types.php">Agreement Types</a>
                                 <a class="dropdown-item" href="../super_admin/all_payment_types.php">Payment Types</a>
+                                <a class="dropdown-item" href="../super_admin/all_appointment_status.php">Appointment Type</a>
                                 <a class="dropdown-item" href="../super_admin/all_interests.php">Interests</a>
                                 <a class="dropdown-item" href="../super_admin/all_document_types.php">Document Types</a>
                                 <a class="dropdown-item" href="../super_admin/all_skill_levels.php">Skill Level</a>
@@ -55,13 +53,29 @@
                                 <a class="dropdown-item" href="../super_admin/manage_help.php">Help</a>
                             </div>
                         </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="javascript:" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Analytics
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="../super_admin/all_chat_section.php">Assistant</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="javascript:" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Operations
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="../super_admin/csv_uploader.php">CSV Uploader</a>
+                            </div>
+                        </li>
                     </ul>
                     <div style="margin-left: 15%;">
                         <?php
                         $currentURL = $_SERVER['REQUEST_URI'];
                         $url = explode("/", $currentURL);
                         $address = $url[2];
-                        if ($address == "all_rate_types.php" || $address == "all_users.php" || $address == "all_departments.php" || $address == "all_roles.php" || $address == "all_countries.php" || $address == "all_states.php"|| $address == "all_currency.php" || $address == "all_account_types.php" || $address == "all_appointment_status.php" || $address == "all_business_types.php" || $address == "csv_uploader.php") { ?>
+                        if ($address == "all_rate_types.php" || $address == "all_users.php" || $address == "all_departments.php" || $address == "all_roles.php" || $address == "all_countries.php" || $address == "all_states.php"|| $address == "all_currency.php" || $address == "all_account_types.php" || $address == "all_business_types.php") { ?>
                             <ul class="nav nav-tabs justify-content-center">
                                 <li class="nav-item"><a class="nav-link" href="../super_admin/all_rate_types.php">Rate Types</a></li>
                                 <li class="nav-item"><a class="nav-link" href="../super_admin/all_users.php">Users</a></li>
@@ -71,15 +85,14 @@
                                 <li class="nav-item"><a class="nav-link" href="../super_admin/all_states.php">States</a></li>
                                 <li class="nav-item"><a class="nav-link" href="../super_admin/all_currency.php">Currency</a></li>
                                 <li class="nav-item"><a class="nav-link" href="../super_admin/all_account_types.php">Account Types</a></li>
-                                <li class="nav-item"><a class="nav-link" href="../super_admin/all_appointment_status.php">Appointment Status</a></li>
                                 <li class="nav-item"><a class="nav-link" href="../super_admin/all_business_types.php">Business Types</a></li>
-                                <li class="nav-item"><a class="nav-link" href="../super_admin/csv_uploader.php">CSV Uploader</a></li>
                             </ul>
-                        <?php } elseif ($address == "all_enrollment_types.php" || $address == "all_agreement_types.php" || $address == "all_payment_types.php" || $address == "all_interests.php" || $address == "all_document_types.php" || $address == "all_skill_levels.php" || $address == "all_relationship.php" || $address == "all_service_class.php" || $address == "all_frequency.php") { ?>
+                        <?php } elseif ($address == "all_enrollment_types.php" || $address == "all_agreement_types.php" || $address == "all_payment_types.php" || $address == "all_appointment_status.php" || $address == "all_interests.php" || $address == "all_document_types.php" || $address == "all_skill_levels.php" || $address == "all_relationship.php" || $address == "all_service_class.php" || $address == "all_frequency.php") { ?>
                             <ul class="nav nav-tabs justify-content-center">
                                 <li class="nav-item"><a class="nav-link" href="../super_admin/all_enrollment_types.php">Enrollment Types</a></li>
                                 <li class="nav-item"><a class="nav-link" href="../super_admin/all_agreement_types.php">Agreement Types</a></li>
                                 <li class="nav-item"><a class="nav-link" href="../super_admin/all_payment_types.php">Payment Types</a></li>
+                                <li class="nav-item"><a class="nav-link" href="../super_admin/all_appointment_status.php">Appointment Type</a></li>
                                 <li class="nav-item"><a class="nav-link" href="../super_admin/all_interests.php">Interests</a></li>
                                 <li class="nav-item"><a class="nav-link" href="../super_admin/all_document_types.php">Document Types</a></li>
                                 <li class="nav-item"><a class="nav-link" href="../super_admin/all_skill_levels.php">Skill Level</a></li>
@@ -97,6 +110,14 @@
                                 <li class="nav-item"><a class="nav-link" href="../super_admin/manage_help_category.php">Help Category</a></li>
                                 <li class="nav-item"><a class="nav-link" href="../super_admin/manage_help_sub_category.php">Help Subcategory</a></li>
                                 <li class="nav-item"><a class="nav-link" href="../super_admin/manage_help.php">Help</a></li>
+                            </ul>
+                        <?php } elseif ($address == "all_chat_section.php") { ?>
+                            <ul class="nav nav-tabs justify-content-center">
+                                <li class="nav-item"><a class="nav-link" href="../super_admin/all_chat_section.php">Assistant</a></li>
+                            </ul>
+                        <?php } elseif ($address == "csv_uploader.php") { ?>
+                            <ul class="nav nav-tabs justify-content-center">
+                                <li class="nav-item"><a class="nav-link" href="../super_admin/csv_uploader.php">CSV Uploader</a></li>
                             </ul>
                         <?php } ?>
                     </div>
---
+++ super_admin/assistant_title.php
@@ -0,0 +1,78 @@
+<?php
+require_once('../global/config.php');
+global $db;
+
+$title = "Assistant Title";
+
+if($_SESSION['PK_USER'] == 0 || $_SESSION['PK_USER'] == '' || $_SESSION['PK_ROLES'] != 1 ){
+    header("location:../login.php");
+    exit;
+}
+
+if(!empty($_POST)){
+    db_perform('DOA_OTHER_SETTING', $_POST, 'update'," PK_OTHER_SETTING =  1");
+    header("location:assistant_title.php");
+}
+
+$res = $db->Execute("SELECT ASSISTANT_TITLE FROM `DOA_OTHER_SETTING` WHERE PK_OTHER_SETTING = 1");
+if($res->RecordCount() == 0){
+    header("location:assistant_title.php");
+    exit;
+}
+$ASSISTANT_TITLE = $res->fields['ASSISTANT_TITLE'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once('../includes/header.php');?>
+<body class="skin-default-dark fixed-layout">
+<?php require_once('../includes/loader.php');?>
+<div id="main-wrapper">
+    <?php require_once('../includes/top_menu.php');?>
+    <div class="page-wrapper">
+        <?php require_once('../includes/top_menu_bar.php') ?>
+        <div class="container-fluid body_content">
+            <div class="row page-titles">
+                <div class="col-md-5 align-self-center">
+                    <h4 class="text-themecolor"><?=$title?></h4>
+                </div>
+                <div class="col-md-7 align-self-center text-end">
+                    <div class="d-flex justify-content-end align-items-center">
+                        <ol class="breadcrumb justify-content-end">
+                            <li class="breadcrumb-item"><a href="setup.php">Setup</a></li>
+                            <li class="breadcrumb-item"><a href="all_chat_section.php">All Chat Section</a></li>
+                            <li class="breadcrumb-item active"><?=$title?></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+
+                            <form class="form-material form-horizontal m-t-30" name="form1" id="form1" action="" method="post" enctype="multipart/form-data">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label class="col-md-12" for="example-text">Assistant Title<span class="text-danger">*</span></label>
+                                            <div class="col-md-12">
+                                                <input type="text" id="ASSISTANT_TITLE" name="ASSISTANT_TITLE" class="form-control" placeholder="Enter Chat Sections" required value="<?php echo $ASSISTANT_TITLE?>">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <button type="submit" class="btn btn-info waves-effect waves-light m-r-10 text-white">Submit</button>
+                                <button type="button" class="btn btn-inverse waves-effect waves-light" onclick="window.location.href='assistant_title.php'">Cancel</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require_once('../includes/footer.php');?>
+</body>
+</html>
