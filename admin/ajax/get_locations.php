f54bcc71abde59d8bbbe760e897ed12fae7d678c
roumya <roumya.karmakar.01@gmail.com>
2024-07-26 23:12:01 +0530

    need to select one location to create any kind of appointment, changed all preferred to primary location

--- admin/ajax/get_locations.php
+++ admin/ajax/get_locations.php
@@ -11,3 +11,8 @@ $row = $db->Execute("SELECT DISTINCT DOA_LOCATION.PK_LOCATION, DOA_LOCATION.LOCA
 while (!$row->EOF) { ?>
     <option value="<?=$row->fields['PK_LOCATION'];?>" data-location_id="<?=$row->fields['PK_LOCATION']?>" <?=($LOCATION_ID == $row->fields['PK_LOCATION'])?'selected':''?>><?=$row->fields['LOCATION_NAME']?></option>
 <?php $row->MoveNext(); } ?>
+<?php
+$row = $db->Execute("SELECT DISTINCT DOA_LOCATION.PK_LOCATION, DOA_LOCATION.LOCATION_NAME FROM DOA_LOCATION LEFT JOIN DOA_USER_MASTER ON DOA_USER_MASTER.PRIMARY_LOCATION_ID=DOA_LOCATION.PK_LOCATION WHERE DOA_USER_MASTER.PK_USER = ".$_POST['PK_USER']." AND DOA_USER_MASTER.PK_ACCOUNT_MASTER = '$_SESSION[PK_ACCOUNT_MASTER]' AND ACTIVE = 1 ORDER BY DOA_LOCATION.LOCATION_NAME");
+while (!$row->EOF) { ?>
+    <option value="<?=$row->fields['PK_LOCATION'];?>" data-location_id="<?=$row->fields['PK_LOCATION']?>" <?=($LOCATION_ID == $row->fields['PK_LOCATION'])?'selected':''?>><?=$row->fields['LOCATION_NAME']?></option>
+    <?php $row->MoveNext(); } ?>
