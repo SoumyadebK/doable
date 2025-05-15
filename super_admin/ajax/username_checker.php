1f22d3cbe01c3a6fb943145ee07cc8a10ee00aad
roumya <roumya.karmakar.01@gmail.com>
2025-05-16 00:53:19 +0530

    New Account

---
+++ super_admin/ajax/username_checker.php
@@ -0,0 +1,48 @@
+<?php
+require_once('../../global/config.php');
+
+if(isset($_POST['USER_NAME'])) {
+    $USER_NAME = $_POST['USER_NAME'];
+    $result = $db->Execute("SELECT * FROM `DOA_USERS` WHERE USER_NAME = '".$USER_NAME."'");
+
+    if($result->RecordCount() == 0){
+        echo '<span class="text-success">Username <b>'.$USER_NAME.'</b> is available!</span>';
+    } else {
+        echo '<span class="text-danger">Username <b>'.$USER_NAME.'</b> is already taken!</span>';
+    }
+}
+
+if(isset($_POST['CUSTOMER_ID'])) {
+    $CUSTOMER_ID = $_POST['CUSTOMER_ID'];
+    $result = $db->Execute("SELECT * FROM `DOA_USERS` WHERE USER_NAME = '".$CUSTOMER_ID."'");
+
+    if($result->RecordCount() == 0){
+        echo '';
+    } else {
+        echo '<span class="text-danger">Username <b>'.$CUSTOMER_ID.'</b> is already taken!</span>';
+    }
+}
+
+if(isset($_POST['PHONE'])) {
+    $PHONE = $_POST['PHONE'];
+    $result = $db->Execute("SELECT * FROM `DOA_USERS` WHERE PHONE = '".$PHONE."'");
+
+    if($result->RecordCount() == 0){
+        echo '';
+    } else {
+        echo '<span class="text-danger"> <b>'.$PHONE.'</b> is already taken!</span>';
+    }
+}
+
+if(isset($_POST['EMAIL_ID'])) {
+    $EMAIL_ID = $_POST['EMAIL_ID'];
+    $result = $db->Execute("SELECT * FROM `DOA_USERS` WHERE EMAIL_ID = '".$EMAIL_ID."'");
+
+    if($result->RecordCount() == 0){
+        echo '';
+    } else {
+        echo '<span class="text-danger"> <b>'.$EMAIL_ID.'</b> is already taken!</span>';
+    }
+}
+
+?>
