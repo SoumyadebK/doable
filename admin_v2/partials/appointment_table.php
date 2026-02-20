18ade901af509763264f0f2dc4fcede4190e3294
soumyadeb1993 <deb.soumya93@gmail.com>
2026-02-21 01:24:20 +0530

    new ui work

--- admin_v2/partials/appointment_table.php
+++ admin_v2/partials/appointment_table.php
@@ -53,60 +53,57 @@ $where_conditions[] = "(CUSTOMER.IS_DELETED = 0 OR CUSTOMER.IS_DELETED IS null)"
 $where_clause = "WHERE " . implode(" AND ", $where_conditions);
 
 $query = "SELECT
-                                DOA_APPOINTMENT_MASTER.PK_APPOINTMENT_MASTER,
-                                DOA_APPOINTMENT_MASTER.STANDING_ID,
-                                DOA_APPOINTMENT_MASTER.PK_ENROLLMENT_SERVICE,
-                                DOA_APPOINTMENT_MASTER.PK_SCHEDULING_CODE,
-                                DOA_APPOINTMENT_MASTER.GROUP_NAME,
-                                DOA_APPOINTMENT_MASTER.SERIAL_NUMBER,
-                                DOA_APPOINTMENT_MASTER.DATE AS DATE,
-                                DATE(DATE) AS APPOINTMENT_DATE,
-                                DAYNAME(DATE) AS DAY_NAME,      
-                                DAY(DATE) AS DAY_NUMBER,        
-                                DOA_APPOINTMENT_MASTER.START_TIME,
-                                DOA_APPOINTMENT_MASTER.END_TIME,
-                                DOA_APPOINTMENT_MASTER.APPOINTMENT_TYPE,
-                                DOA_APPOINTMENT_MASTER.IS_PAID,
-                                DOA_ENROLLMENT_MASTER.ENROLLMENT_NAME,
-                                DOA_ENROLLMENT_MASTER.ENROLLMENT_ID,
-                                DOA_SERVICE_MASTER.SERVICE_NAME,
-                                DOA_SERVICE_CODE.SERVICE_CODE,
-                                DOA_APPOINTMENT_MASTER.IS_PAID,
-                                DOA_APPOINTMENT_MASTER.IS_CHARGED,
-                                DOA_APPOINTMENT_MASTER.APPOINTMENT_TYPE,
-                                DOA_APPOINTMENT_MASTER.PK_APPOINTMENT_STATUS,
-                                DOA_APPOINTMENT_STATUS.APPOINTMENT_STATUS,
-                                DOA_APPOINTMENT_STATUS.STATUS_CODE,
-                                DOA_APPOINTMENT_STATUS.COLOR_CODE AS APPOINTMENT_COLOR,
-                                DOA_APPOINTMENT_STATUS.APPOINTMENT_STATUS,
-                                DOA_SCHEDULING_CODE.COLOR_CODE,
-                                GROUP_CONCAT(DISTINCT(CONCAT(SERVICE_PROVIDER.FIRST_NAME, ' ', SERVICE_PROVIDER.LAST_NAME)) SEPARATOR ', ') AS SERVICE_PROVIDER_NAME,
-                                GROUP_CONCAT(DISTINCT(CONCAT(CUSTOMER.FIRST_NAME, ' ', CUSTOMER.LAST_NAME)) SEPARATOR ', ') AS CUSTOMER_NAME
-                            FROM
-                                DOA_APPOINTMENT_MASTER
-                            LEFT JOIN DOA_APPOINTMENT_SERVICE_PROVIDER ON DOA_APPOINTMENT_MASTER.PK_APPOINTMENT_MASTER = DOA_APPOINTMENT_SERVICE_PROVIDER.PK_APPOINTMENT_MASTER
-                            LEFT JOIN $master_database.DOA_USERS AS SERVICE_PROVIDER ON DOA_APPOINTMENT_SERVICE_PROVIDER.PK_USER = SERVICE_PROVIDER.PK_USER
-                            
-                            LEFT JOIN DOA_APPOINTMENT_CUSTOMER ON DOA_APPOINTMENT_MASTER.PK_APPOINTMENT_MASTER = DOA_APPOINTMENT_CUSTOMER.PK_APPOINTMENT_MASTER
-                            LEFT JOIN $master_database.DOA_USER_MASTER AS DOA_USER_MASTER ON DOA_APPOINTMENT_CUSTOMER.PK_USER_MASTER = DOA_USER_MASTER.PK_USER_MASTER
-                            LEFT JOIN $master_database.DOA_USERS AS CUSTOMER ON DOA_USER_MASTER.PK_USER = CUSTOMER.PK_USER
-                                    
-                            LEFT JOIN DOA_SCHEDULING_CODE ON DOA_APPOINTMENT_MASTER.PK_SCHEDULING_CODE = DOA_SCHEDULING_CODE.PK_SCHEDULING_CODE
-                            LEFT JOIN DOA_SERVICE_MASTER ON DOA_APPOINTMENT_MASTER.PK_SERVICE_MASTER = DOA_SERVICE_MASTER.PK_SERVICE_MASTER
-                            LEFT JOIN $master_database.DOA_APPOINTMENT_STATUS AS DOA_APPOINTMENT_STATUS ON DOA_APPOINTMENT_MASTER.PK_APPOINTMENT_STATUS = DOA_APPOINTMENT_STATUS.PK_APPOINTMENT_STATUS 
-                            LEFT JOIN DOA_ENROLLMENT_MASTER ON DOA_APPOINTMENT_MASTER.PK_ENROLLMENT_MASTER = DOA_ENROLLMENT_MASTER.PK_ENROLLMENT_MASTER
-                            LEFT JOIN DOA_SERVICE_CODE ON DOA_APPOINTMENT_MASTER.PK_SERVICE_CODE = DOA_SERVICE_CODE.PK_SERVICE_CODE
-                            $where_clause
-                            GROUP BY DOA_APPOINTMENT_MASTER.PK_APPOINTMENT_MASTER
-                            ORDER BY DOA_APPOINTMENT_MASTER.START_TIME ASC";
+            DOA_APPOINTMENT_MASTER.PK_APPOINTMENT_MASTER,
+            DOA_APPOINTMENT_MASTER.STANDING_ID,
+            DOA_APPOINTMENT_MASTER.PK_ENROLLMENT_SERVICE,
+            DOA_APPOINTMENT_MASTER.PK_SCHEDULING_CODE,
+            DOA_APPOINTMENT_MASTER.GROUP_NAME,
+            DOA_APPOINTMENT_MASTER.SERIAL_NUMBER,
+            DOA_APPOINTMENT_MASTER.DATE AS DATE,
+            DATE(DATE) AS APPOINTMENT_DATE,
+            DAYNAME(DATE) AS DAY_NAME,      
+            DAY(DATE) AS DAY_NUMBER,        
+            DOA_APPOINTMENT_MASTER.START_TIME,
+            DOA_APPOINTMENT_MASTER.END_TIME,
+            DOA_APPOINTMENT_MASTER.APPOINTMENT_TYPE,
+            DOA_APPOINTMENT_MASTER.IS_PAID,
+            DOA_ENROLLMENT_MASTER.ENROLLMENT_NAME,
+            DOA_ENROLLMENT_MASTER.ENROLLMENT_ID,
+            DOA_SERVICE_MASTER.SERVICE_NAME,
+            DOA_SERVICE_CODE.SERVICE_CODE,
+            DOA_APPOINTMENT_MASTER.IS_PAID,
+            DOA_APPOINTMENT_MASTER.IS_CHARGED,
+            DOA_APPOINTMENT_MASTER.APPOINTMENT_TYPE,
+            DOA_APPOINTMENT_MASTER.PK_APPOINTMENT_STATUS,
+            DOA_APPOINTMENT_STATUS.APPOINTMENT_STATUS,
+            DOA_APPOINTMENT_STATUS.STATUS_CODE,
+            DOA_APPOINTMENT_STATUS.COLOR_CODE AS APPOINTMENT_COLOR,
+            DOA_APPOINTMENT_STATUS.APPOINTMENT_STATUS,
+            DOA_SCHEDULING_CODE.COLOR_CODE,
+            GROUP_CONCAT(DISTINCT(CONCAT(SERVICE_PROVIDER.FIRST_NAME, ' ', SERVICE_PROVIDER.LAST_NAME)) SEPARATOR ', ') AS SERVICE_PROVIDER_NAME,
+            GROUP_CONCAT(DISTINCT(CONCAT(CUSTOMER.FIRST_NAME, ' ', CUSTOMER.LAST_NAME)) SEPARATOR ', ') AS CUSTOMER_NAME
+        FROM
+            DOA_APPOINTMENT_MASTER
+        LEFT JOIN DOA_APPOINTMENT_SERVICE_PROVIDER ON DOA_APPOINTMENT_MASTER.PK_APPOINTMENT_MASTER = DOA_APPOINTMENT_SERVICE_PROVIDER.PK_APPOINTMENT_MASTER
+        LEFT JOIN $master_database.DOA_USERS AS SERVICE_PROVIDER ON DOA_APPOINTMENT_SERVICE_PROVIDER.PK_USER = SERVICE_PROVIDER.PK_USER
+        
+        LEFT JOIN DOA_APPOINTMENT_CUSTOMER ON DOA_APPOINTMENT_MASTER.PK_APPOINTMENT_MASTER = DOA_APPOINTMENT_CUSTOMER.PK_APPOINTMENT_MASTER
+        LEFT JOIN $master_database.DOA_USER_MASTER AS DOA_USER_MASTER ON DOA_APPOINTMENT_CUSTOMER.PK_USER_MASTER = DOA_USER_MASTER.PK_USER_MASTER
+        LEFT JOIN $master_database.DOA_USERS AS CUSTOMER ON DOA_USER_MASTER.PK_USER = CUSTOMER.PK_USER
+                
+        LEFT JOIN DOA_SCHEDULING_CODE ON DOA_APPOINTMENT_MASTER.PK_SCHEDULING_CODE = DOA_SCHEDULING_CODE.PK_SCHEDULING_CODE
+        LEFT JOIN DOA_SERVICE_MASTER ON DOA_APPOINTMENT_MASTER.PK_SERVICE_MASTER = DOA_SERVICE_MASTER.PK_SERVICE_MASTER
+        LEFT JOIN $master_database.DOA_APPOINTMENT_STATUS AS DOA_APPOINTMENT_STATUS ON DOA_APPOINTMENT_MASTER.PK_APPOINTMENT_STATUS = DOA_APPOINTMENT_STATUS.PK_APPOINTMENT_STATUS 
+        LEFT JOIN DOA_ENROLLMENT_MASTER ON DOA_APPOINTMENT_MASTER.PK_ENROLLMENT_MASTER = DOA_ENROLLMENT_MASTER.PK_ENROLLMENT_MASTER
+        LEFT JOIN DOA_SERVICE_CODE ON DOA_APPOINTMENT_MASTER.PK_SERVICE_CODE = DOA_SERVICE_CODE.PK_SERVICE_CODE
+        $where_clause
+        GROUP BY DOA_APPOINTMENT_MASTER.PK_APPOINTMENT_MASTER
+        ORDER BY DOA_APPOINTMENT_MASTER.START_TIME ASC";
 
-// Debug: Uncomment to see the query
-// echo "<!-- Query: " . htmlspecialchars($query) . " -->";
 
 $appointments = $db_account->Execute($query);
 
 $current_date = '';
-
 // First, count appointments per date
 $temp_appointments = $db_account->Execute($query);
 $date_counts = [];
@@ -120,7 +117,6 @@ while (!$temp_appointments->EOF) {
 }
 
 // Reset the result set
-$appointments = $db_account->Execute($query);
 
 while (!$appointments->EOF) {
     $appointment_date = $appointments->fields['DATE'];
@@ -132,7 +128,8 @@ while (!$appointments->EOF) {
     $end_time = date('h:i A', strtotime($appointments->fields['END_TIME']));
     $status = $appointments->fields['APPOINTMENT_STATUS'];
     $status_color = $appointments->fields['APPOINTMENT_COLOR'];
-    $service_provider = $appointments->fields['SERVICE_PROVIDER_NAME'];
+    $service_provider = ($appointments->fields['SERVICE_PROVIDER_NAME'] == null) ? '' : $appointments->fields['SERVICE_PROVIDER_NAME'];
+    $CUSTOMER_NAME = ($appointments->fields['CUSTOMER_NAME'] == null) ? '' : $appointments->fields['CUSTOMER_NAME'];
     $service_name = $appointments->fields['SERVICE_NAME'];
     $service_code = $appointments->fields['SERVICE_CODE'];
     $color_code = $appointments->fields['COLOR_CODE'];
@@ -143,18 +140,26 @@ while (!$appointments->EOF) {
     $title_display = '';
     if ($appointment_type == 'NORMAL') {
         $title_display = 'Private Session';
+        $TYPE = 'appointment';
     } elseif ($appointment_type == 'AD-HOC') {
         $title_display = 'Ad-Hoc';
+        $TYPE = 'appointment';
     } elseif ($appointment_type == 'GROUP') {
         $title_display = 'Group Class';
+        $TYPE = 'group_class';
     } elseif ($appointment_type == 'DEMO') {
         $title_display = 'Record Only';
+        $TYPE = 'appointment';
     } else {
         $title_display = $appointments->fields['ENROLLMENT_NAME'] ?: $service_name;
     }
 
+    $customer = getProfileBadge($CUSTOMER_NAME);
+    $customer_initial = $customer['initials'];
+    $customer_color = $customer['color'];
+
     $profile = getProfileBadge($service_provider);
-    $profile_name = $profile['initials'];
+    $profile_initial = $profile['initials'];
     $profile_color = $profile['color'];
 ?>
     <tr style="height: 55px;">
@@ -171,7 +176,10 @@ while (!$appointments->EOF) {
             </td>
         <?php endif; ?>
 
-        <td style="vertical-align: middle;"><?= $title_display; ?></td>
+        <td style="vertical-align: middle;">
+            <span class="avatarname" style="color: #fff; background-color: <?= $customer_color ?>;"><?= $customer_initial; ?></span>
+            <?= $CUSTOMER_NAME ?>
+        </td>
         <td style="vertical-align: middle;"><?= $start_time . ' – ' . $end_time; ?></td>
         <td style="vertical-align: middle;">
             <span class="status not-started" style="background-color: <?= $status_color ?>20 !important; color: <?= $status_color ?> !important;">
@@ -183,14 +191,14 @@ while (!$appointments->EOF) {
             </span>
         </td>
         <td style="vertical-align: middle;">
-            <span class="avatarname" style="color: #fff; background-color: <?= $profile_color ?>;"><?= $profile_name; ?></span>
+            <span class="avatarname" style="color: #fff; background-color: <?= $profile_color ?>;"><?= $profile_initial; ?></span>
             <?= $service_provider ?>
         </td>
         <td style="vertical-align: middle;">
             <?= $service_name ?>&nbsp;&nbsp;<span class="badge ms-auto" style="font-size: 12px !important; background-color: <?= $color_code ?>20 !important; color: <?= $color_code ?> !important;"><?= $service_code ?></span>
         </td>
         <td class="text-center" style="vertical-align: middle;">
-            <button type="button" class="bg-transparent p-0 border-0" onclick="loadViewAppointmentModal(<?= $appointment_id; ?>)">
+            <button type="button" class="bg-transparent p-0 border-0" onclick="loadViewAppointmentModal(<?= $appointment_id; ?>, <?= $TYPE ?>)">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="1rem" height="1rem" fill="CurrentColor">
                     <circle cx="256" cy="256" r="48" />
                     <circle cx="256" cy="416" r="48" />
