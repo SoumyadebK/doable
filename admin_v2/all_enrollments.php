d2c86ab31ef8d010737daea6d2d156b57a27e9ba
Roumyadeb Karmakar <roumya.karmakar.01@gmail.com>
2026-04-13 23:11:11 +0530

    13-04

--- admin_v2/all_enrollments.php
+++ admin_v2/all_enrollments.php
@@ -420,6 +420,7 @@ if (isset($_POST['SUBMIT'])) {
 <html lang="en">
 <?php include 'layout/header_script.php'; ?>
 <?php include 'layout/header.php'; ?>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 
 <body class="skin-default-dark fixed-layout">
     <?php //require_once('../includes/loader.php'); 
@@ -666,6 +667,19 @@ if (isset($_POST['SUBMIT'])) {
                                         </td>
                                         <td>$<?= str_replace(",", "", number_format($enrollment_data->fields['TOTAL_AMOUNT'], 2)) ?></td>
                                         <td class="text-center" style="vertical-align: middle;">
+                                            <?php if (in_array('Enrollments Delete', $PERMISSION_ARRAY)) { ?>
+                                                <a href="javascript:;" onclick="ConfirmDelete(<?= $enrollment_data->fields['PK_ENROLLMENT_MASTER'] ?>);" title="Delete" style="font-size:15px"><i class="fa fa-trash"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                            <?php } ?>
+
+                                            <?php if ($_SESSION['PK_ROLES'] != 5) {
+                                                if ($enrollment_data->fields['STATUS'] == 'A') { ?>
+                                                    <a href="javascript:;" onclick="cancelEnrollment(<?= $enrollment_data->fields['PK_ENROLLMENT_MASTER'] ?>, <?= $enrollment_data->fields['PK_USER_MASTER'] ?>)" style="font-size:15px"><i class="fa fa-ban"></i></a>
+                                                <?php } elseif ($enrollment_data->fields['STATUS'] == 'C') { ?>
+                                                    <p style="color: red;">Cancelled</p>
+                                                    <!--<a href="all_enrollments.php?id=<?php /*=$row->fields['PK_ENROLLMENT_MASTER']*/ ?>&status=active">Active Enrollment</a>-->
+                                            <?php }
+                                            } ?>
+
                                             <a href="../admin/enrollment.php?id=<?= $enrollment_data->fields['PK_ENROLLMENT_MASTER'] ?>">
                                                 <button type="button" class="bg-transparent p-0 border-0">
                                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="1rem" height="1rem" fill="CurrentColor">
