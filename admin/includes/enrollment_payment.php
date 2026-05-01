2b91c67c54b19165ed93e4e01489273a1cb849bd
soumyadeb1993 <deb.soumya93@gmail.com>
2026-05-01 11:01:37 +0530

    fixes

--- admin/includes/enrollment_payment.php
+++ admin/includes/enrollment_payment.php
@@ -89,6 +89,53 @@
                             </div>
                         </div>
 
+
+
+
+                        <div class="row">
+                            <div class="col-3">
+                                <div class="form-group">
+                                    <label class="form-label">Tip Percentage</label>
+                                    <div class="col-md-12">
+                                        <select class="form-control" name="TIP_PERCENTAGE" id="TIP_PERCENTAGE" onchange="calculateTipAmount()">
+                                            <option value="">Select</option>
+                                            <option value="18">18%</option>
+                                            <option value="20">20%</option>
+                                            <option value="22">22%</option>
+                                            <option value="custom">Custom</option>
+                                        </select>
+                                        <input type="number" class="form-control mt-2" name="CUSTOM_TIP_PERCENTAGE" id="CUSTOM_TIP_PERCENTAGE" placeholder="Enter custom tip percentage" style="display: none;" onkeyup="calculateTipAmount()">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="form-group">
+                                    <label class="form-label">Tip Amount</label>
+                                    <div class="col-md-12">
+                                        <input type="text" class="form-control" name="TIP_AMOUNT" id="TIP_AMOUNT" placeholder="Tip amount" readonly>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div>
+                                    <label class="form-label">Tip <?= $service_provider_title ?></label>
+                                    <div class="col-md-12">
+                                        <select name="PK_SERVICE_PROVIDER[]" id="PK_SERVICE_PROVIDER" multiple>
+                                            <?php
+                                            $row = getServiceProvider();
+                                            while (!$row->EOF) { ?>
+                                                <option value="<?= $row->fields['PK_USER']; ?>"><?= $row->fields['NAME'] ?></option>
+                                            <?php $row->MoveNext();
+                                            } ?>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
                         <?php if ($PAYMENT_GATEWAY == 'Stripe') { ?>
                             <div class="row" id="card_list">
                             </div>
@@ -531,6 +578,15 @@
 <?php } ?>
 
 <script>
+    $('#PK_SERVICE_PROVIDER').SumoSelect({
+        placeholder: 'Select <?= $service_provider_title ?>',
+        search: true,
+        searchText: 'Search...',
+        selectAll: true,
+        captionFormatAllSelected: "All <?= $service_provider_title ?>s are selected!",
+    });
+
+
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
@@ -847,6 +903,25 @@
                 $('#REMAINING_AMOUNT').val(0);
             } else {
                 $('#REMAINING_AMOUNT').val(ACTUAL_AMOUNT - AMOUNT_TO_PAY);
+            }
+        }
+    }
+
+    function calculateTipAmount() {
+        let tipPercentage = $('#TIP_PERCENTAGE').val();
+        if (tipPercentage === 'custom') {
+            $('#CUSTOM_TIP_PERCENTAGE').show();
+            let ACTUAL_AMOUNT = parseFloat($('#ACTUAL_AMOUNT').val());
+            let tipAmount = (ACTUAL_AMOUNT * parseFloat($('#CUSTOM_TIP_PERCENTAGE').val())) / 100;
+            $('#TIP_AMOUNT').val(tipAmount.toFixed(2));
+        } else {
+            $('#CUSTOM_TIP_PERCENTAGE').hide();
+            if (tipPercentage) {
+                let ACTUAL_AMOUNT = parseFloat($('#ACTUAL_AMOUNT').val());
+                let tipAmount = (ACTUAL_AMOUNT * parseFloat(tipPercentage)) / 100;
+                $('#TIP_AMOUNT').val(tipAmount.toFixed(2));
+            } else {
+                $('#TIP_AMOUNT').val(0);
             }
         }
     }
