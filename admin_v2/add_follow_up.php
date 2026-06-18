2a193140032f6e58ca6c89669056ab85e0bfcf80
soumyadeb1993 <deb.soumya93@gmail.com>
2026-06-19 00:53:40 +0530

    fixes

--- admin_v2/add_follow_up.php
+++ admin_v2/add_follow_up.php
@@ -925,6 +925,8 @@ if (!empty($_GET['id'])) {
     <input type="hidden" name="MESSAGE_NOTIFICATIONS" id="MESSAGE_NOTIFICATIONS" value='<?= htmlspecialchars(json_encode($MESSAGE_NOTIFICATIONS)) ?>'>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
     <script>
         // Parse existing data
         let reminders = <?= json_encode($CUSTOM_REMINDERS) ?>;
